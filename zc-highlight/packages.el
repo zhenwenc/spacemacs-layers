bef20bc11123aed5b399b30036ebe61b909e83e2
Frederick Cai <frederick.cai@gmail.com>
2017-03-31 19:27:26 +1300

    highlight: bye hightlight-symbol

--- zc-highlight/packages.el
+++ zc-highlight/packages.el
@@ -1,26 +1,42 @@
 ;;; packages.el --- highlight Layer packages File for My Spacemacs
 
 (defconst zc-highlight-packages
-  '(highlight-symbol))
+  '(highlight-thing))
 
-(defun zc-highlight/init-highlight-symbol ()
-  (use-package highlight-symbol
+(defun zc-highlight/init-highlight-thing ()
+  (use-package highlight-thing
     :defer t
     :init
-    (setq highlight-symbol-idle-delay 0.35)
-    (define-globalized-minor-mode global-highlight-symbol-mode highlight-symbol-mode
-      (lambda ()
-        (when (not (memq major-mode
-                         (list
-                          'magit-status-mode
-                          'magit-diff-mode
-                          'magit-revision-mode
-                          'magit-auto-revert-mode
-                          'magit-reflog-mode
-                          'magit-log-mode
-                          'magit-popup-mode
-                          'tide-references-mode
-                          'minibuffer-inactive-mode
-                          'spacemacs-buffer-mode)))
-          (highlight-symbol-mode 1))))
-    (global-highlight-symbol-mode 1)))
+    (progn
+      (setq highlight-thing-delay-seconds 0.3)
+      (setq highlight-thing-case-sensitive-p t)
+      (set-face-background 'hi-yellow "#3e4446")
+      (set-face-foreground 'hi-yellow nil)
+      (global-highlight-thing-mode 1)
+      )))
+
+;; =======================================================================================
+;; NOTE: highlight-symbol uses font-lock, which problematic in many
+;;       mayjor modes, esp. minibuffers.
+
+;; (defun zc-highlight/init-highlight-symbol ()
+;;   (use-package highlight-symbol
+;;     :defer t
+;;     :init
+;;     (setq highlight-symbol-idle-delay 0.35)
+;;     (define-globalized-minor-mode global-highlight-symbol-mode highlight-symbol-mode
+;;       (lambda ()
+;;         (when (not (memq major-mode
+;;                          (list
+;;                           'magit-status-mode
+;;                           'magit-diff-mode
+;;                           'magit-revision-mode
+;;                           'magit-auto-revert-mode
+;;                           'magit-reflog-mode
+;;                           'magit-log-mode
+;;                           'magit-popup-mode
+;;                           'tide-references-mode
+;;                           'minibuffer-inactive-mode
+;;                           'spacemacs-buffer-mode)))
+;;           (highlight-symbol-mode 1))))
+;;     (global-highlight-symbol-mode 1)))
