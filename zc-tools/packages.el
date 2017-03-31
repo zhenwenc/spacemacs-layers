5dee6f9fb3c5724920e0f84d399ffbcb66a50d0e
Frederick Cai <frederick.cai@gmail.com>
2017-03-31 20:03:11 +1300

    tools: add layer

---
+++ zc-tools/packages.el
@@ -0,0 +1,19 @@
+;;; packages.el --- zc-tools layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Frederick Cai <fredc@Fredericks-MacBook-Pro.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst zc-tools-packages
+  '())
+
+
+
+;;; packages.el ends here
