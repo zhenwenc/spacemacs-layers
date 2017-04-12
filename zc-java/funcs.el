83b68529b22e11a9d2ac587b6485eb2e3433374d
Frederick Cai <frederick.cai@gmail.com>
2017-04-13 11:54:52 +1200

    java: update styles

---
+++ zc-java/funcs.el
@@ -0,0 +1,41 @@
+;;; packages.el --- Java functions File for Spacemacs
+
+
+(defun zc-java/setup-ensime-eldoc ()
+  "Setup ENSIME eldoc."
+  (setq-local eldoc-documentation-function
+              (lambda ()
+                (when (ensime-connected-p)
+                  (ensime-print-type-at-point))))
+  (eldoc-mode))
+
+(defun zc-java/setup-indentation-style ()
+  "Setup Java indentation style."
+  (subword-mode)
+
+  ;; Using Google's style
+  ;; https://github.com/google/styleguide/blob/gh-pages/google-c-style.el
+  (google-set-c-style)
+  (google-make-newline-indent)
+
+  ;; Customize indentation
+  ;; Use (c-show-syntactic-information) to check syntax element
+  ;; https://www.gnu.org/software/emacs/manual/html_node/ccmode/c_002doffsets_002dalist.html
+  (setq c-basic-offset 4)
+
+  (c-set-offset 'statement-cont '+)
+  (c-set-offset 'inexpr-class '0)
+  )
+
+(defun zc-java/ensime-print-errors-at-point (&optional arg)
+  (interactive "P")
+  (let ((msgs (append (ensime-errors-at (point)))))
+    (when msgs
+      (let ((msg (mapconcat 'identity msgs "\n")))
+        (when (equal arg '(16))
+          (ensime--make-result-overlay
+              (format "%S" msg)
+            :where (point)
+            :duration 'command))
+        (message "%s" msg)))
+    (ensime-event-sig :errors-at-point-printed)))
--- zc-java/packages.el
+++ zc-java/packages.el
@@ -5,7 +5,8 @@
 (defconst zc-java-packages
   '(aggressive-indent
     ensime
-    (java-mode :location built-in)))
+    (java-mode :location built-in)
+    (google-c-style :location local)))
 
 (defun zc-java/post-init-aggressive-indent ()
   (use-package aggressive-indent
@@ -13,30 +14,59 @@
     (progn
       (add-to-list 'aggressive-indent-excluded-modes 'java-mode))))
 
+(defun zc-java/post-init-eldoc ()
+  (add-hook 'java-mode-local-vars-hook #'zc-java/setup-ensime-eldoc))
+
+(defun zc-java/init-google-c-style ()
+  (use-package google-c-style
+    :defer t
+    :commands (google-c-style google-set-c-style)
+    :init
+    (add-hook 'c-mode-common-hook #'zc-java/setup-indentation-style)))
+
 (defun zc-java/init-java-mode ()
   (use-package java-mode
-    :defer t
-    :mode ("\\.java\\'" . java-mode)
-    ))
-
-(defun zc-java/post-init-ensime ()
-  (use-package ensime
     :defer t
     :init
     (progn
       (add-hook 'java-mode-hook #'zc-ensime/setup-ensime)
 
-      (dolist (prefix '(("me" . "java/errors")
-                        ("md" . "java/eclimd")
-                        ("mf" . "java/find")
-                        ("mg" . "java/goto")
-                        ("mr" . "java/refactor")
-                        ("mh" . "java/documentation")
-                        ("mm" . "java/maven")
-                        ("ma" . "java/ant")
-                        ("mp" . "java/project")
-                        ("mt" . "java/test")))
-        (spacemacs/declare-prefix-for-mode 'java-mode (car prefix) (cdr prefix))))
+      (put 'java-backend 'safe-local-variable 'symbolp)
+
+      ;; Define command prefixes
+      (evil-define-key 'normal java-mode-map
+        (kbd "TAB") 'indent-for-tab-command
+        (kbd "C-i") 'indent-for-tab-command)
+
+      ;; Define command prefixes
+      (setq zc-java/key-binding-prefixes '(("me" . "errors")
+                                           ("md" . "eclimd")
+                                           ("mf" . "find")
+                                           ("mg" . "goto")
+                                           ("mr" . "refactor")
+                                           ("mh" . "documentation")
+                                           ("mm" . "maven")
+                                           ("ma" . "ant")
+                                           ("mp" . "project")
+                                           ("mt" . "test")))
+      (mapc (lambda(x) (spacemacs/declare-prefix-for-mode
+                         'java-mode (car x) (cdr x)))
+            zc-java/key-binding-prefixes))
+    ))
+
+(defun zc-java/post-init-ensime ()
+  (use-package ensime
+    :defer t
+    :config
+    (progn
+      (evil-define-key 'normal ensime-mode-map
+        ;; The type inspector for Java project has problem
+        (kbd "RET") 'ensime-type-at-point)
+
+      ;; NOTE: see ensime-emacs issue #408
+      (spacemacs/set-leader-keys-for-major-mode 'java-mode
+        "ee" 'zc-java/ensime-print-errors-at-point)
+      )
     ))
 
 ;;; packages.el ends here
